d80cc9f9b312d394c6daf78035a15fa0ea518ee3
naveen-53 <naveennk5302@gmail.com>
2025-12-18 19:32:46 +0530

    DB added

--- src/main/java/com/example/QRGenerator.java
+++ src/main/java/com/example/QRGenerator.java
@@ -5,13 +5,55 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class QRGenerator {
 
     private static int count = 1;
+
+    public static void saveToDatabase(String text, byte[] imageBytes) {
+
+        String url = "jdbc:mysql://localhost:3306/qr";
+        String user = "root";
+        String password = "1234";
+
+        String sql = "INSERT INTO qr_codes (qr_text, qr_image) VALUES (?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, text);
+            ps.setBytes(2, imageBytes);
+            ps.executeUpdate();
+
+            System.out.println("QR Code stored in database");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    
+
+    public static byte[] generateQRCodeBytes(String text, int width, int height)
+            throws Exception {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix =  qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", baos);
+
+        return baos.toByteArray();
+    }
+
 
     public static void generateQRCode(String text, int width, int height) throws Exception {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
@@ -31,15 +73,14 @@ public class QRGenerator {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Enter URL (or type 'exit'): ");
+            System.out.print("Enter URL (or exit): ");
             String text = sc.next();
 
-            if (text.equalsIgnoreCase("exit")) {
-                break;
-            }
+            if (text.equalsIgnoreCase("exit")) break;
 
             try {
-                generateQRCode(text, 300, 300);
+                byte[] qrBytes = generateQRCodeBytes(text, 300, 300);
+                saveToDatabase(text, qrBytes);
             } catch (Exception e) {
                 e.printStackTrace();
             }
